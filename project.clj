3348f3c002b1806c810f7b589c389bcdf0e16dc0
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-05-01 22:30:44 -0700

    Update project metadata.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject emgine "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Multi-language code generation from clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]])
